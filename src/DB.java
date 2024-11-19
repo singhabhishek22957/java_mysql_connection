d16c3058460c9da7354bbbb712914f312d1dbfbf
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-11-19 15:30:26 +0530

    add update student by id

--- src/DB.java
+++ src/DB.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class DB {
 
@@ -28,6 +29,26 @@ public class DB {
             System.out.println("Error to insert student Data: "+e);
         }
     }
+    public  void getStudentById (int  s_id ) {
+        try {
+            String query = "SELECT * FROM student WHERE id = ?;";
+
+            Connection conn = DriverManager.getConnection(url,user,dbpassword);
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1,s_id);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()){
+                System.out.println("ID: "+rs.getInt("id")+", Name: "+
+                        rs.getString("name")+", Course: "+rs.getString("course")+", Age: "+
+                        rs.getInt("age")+", Phone Number: "+rs.getString("phone"));
+            }
+
+
+
+        }catch (Exception e){
+            System.out.println("Error to insert student Data: "+e);
+        }
+    }
     public  void studentByCourse (String  s_course ) {
         try {
             String query = "SELECT * FROM student WHERE course = ?;";
@@ -49,7 +70,6 @@ public class DB {
         }
     }
 
-
     public  void getAllStudent () {
         try {
             String query = "select *from student";
@@ -69,6 +89,69 @@ public class DB {
             System.out.println("Error to insert student Data: "+e);
         }
     }
+
+    public  void updateStudent (int s_id,String s_name , String s_course , Integer s_age,String s_phone) {
+//        Scanner sc = new Scanner(System.in);
+//        int s_id = sc.nextInt();
+
+//        System.out.println("Enter your id to update your profile ");
+
+
+        try {
+
+            String queryCheck = "select * from student where id = ? limit 1";
+
+            Connection conn = DriverManager.getConnection(url,user,dbpassword);
+            PreparedStatement pstmt = conn.prepareStatement(queryCheck);
+
+
+            pstmt.setInt(1,s_id);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+            if(s_name==null){
+                s_name = rs.getString("name");
+            }
+            if(s_age==null){
+                s_age = rs.getInt("age");
+            }
+            if(s_course==null){
+                s_course = rs.getString("course");
+            }
+            if(s_phone==null){
+                s_phone = rs.getString("phone");
+            }
+            }else {
+                System.out.println("student not found");
+                return;
+            }
+            String queryUpdate = "UPDATE student SET name = ?, course = ?, age =? , phone =?  WHERE id = ? limit 1";
+            PreparedStatement pstmt1 = conn.prepareStatement(queryUpdate);
+//            ResultSet rs = pstmt.executeQuery(query1);
+                pstmt1.setString(1,s_name);
+                pstmt1.setString(2,s_course);
+                pstmt1.setInt(3,s_age);
+                pstmt1.setString(4,s_phone);
+                pstmt1.setInt(5,s_id);
+                int rowsAffected = pstmt1.executeUpdate();
+                System.out.println("update " + rowsAffected + " row(s) into the table.");
+
+
+
+
+
+            rs.close();
+            pstmt1.close();
+            pstmt.close();
+            conn.close();
+
+        }catch (Exception e){
+            System.out.println("Error to update  student Data: "+e);
+        }
+    }
+
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("List of All USCS Student :-");
+//        System.out.println("List of All USCS Student :-");
         DB db = new DB();
+////        db.getAllStudent();
+////        db.insertStudent("Sonu Pandit","BCA",45,"1234567890");
 //        db.getAllStudent();
-//        db.insertStudent("Sonu Pandit","BCA",45,"1234567890");
-        db.getAllStudent();
-        System.out.println("\n\n\n\n\n MCA student list  ");
-        db.studentByCourse("BCA");
+//        System.out.println("\n\n\n\n\n MCA student list  ");
+//        db.studentByCourse("BCA");
+
+        db.getStudentById(10);
+        db.updateStudent(10,null,"LLB",null,"4561237890");
+        db.getStudentById(10);
 
     }
 
